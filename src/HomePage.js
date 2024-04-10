6de77d94ebd904632541f5c2c30d2f22a5149f31
Tung <gamer1412all@gmail.com>
2024-04-11 01:44:41 +0700

    update

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const HomePage = () =>{
+    return (
+        <p class="thu">
+            giỏi quá nhỉ
+        </p>
+    );
+};
+
+export default HomePage
