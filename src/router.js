19e66f9203b842983adc506be6a27138f719e4f1
Tung <gamer1412all@gmail.com>
2024-04-28 12:02:57 +0700

    bước đầu xây dựng UI cho trang profile

--- src/router.js
+++ src/router.js
@@ -14,6 +14,10 @@ const renderUserRouter = () => {
     {
       path: ROUTERS.USER.PROFILE,
       component: <ProfilePage/>
+    },
+    {
+      path: `${ROUTERS.USER.PROFILE}/change-information`,
+      component: <HomePage/>
     }
   ];
 
