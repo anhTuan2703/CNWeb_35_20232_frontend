a48c228a81f71e4d89ee9d4046e75b1d775361a5
Tung <gamer1412all@gmail.com>
2024-05-09 20:18:49 +0700

    bước đầu làm địa chỉ với ngân hàng

--- src/pages/user/profilePage/accountPage.js
+++ src/pages/user/profilePage/accountPage.js
@@ -1,19 +1,42 @@
-import { memo } from "react";
+import React, { memo, useState } from "react";
 import "./style.css";
 
 const AccountPage = () => {
+  const [bank, setBank] = useState(""); // State cho ngân hàng liên kết
+  const [address, setAddress] = useState(""); // State cho địa chỉ
+
+  const handleBankChange = (event) => {
+    setBank(event.target.value);
+  };
+
+  const handleAddressChange = (event) => {
+    setAddress(event.target.value);
+  };
+
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-3">
-            trái
           </div>
           <div className="col-6">
-            giữa
+            <label htmlFor="bank">Ngân hàng liên kết:</label>
+            <input
+              type="text"
+              id="bank"
+              value={bank}
+              onChange={handleBankChange}
+            />
+            <label htmlFor="address">Địa chỉ:</label>
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={handleAddressChange}
+            />
           </div>
           <div className="col-3">
-            phải
+            {/* Thêm các thông tin khác nếu cần */}
           </div>
         </div>
       </div>
