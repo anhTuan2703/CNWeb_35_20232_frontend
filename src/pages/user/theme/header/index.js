80a363635f5e017953c41d9d6a56f08e3fcf5cf9
Tung <gamer1412all@gmail.com>
2024-05-19 07:02:55 +0700

    done live searching

--- src/pages/user/theme/header/index.js
+++ src/pages/user/theme/header/index.js
@@ -5,8 +5,7 @@ import { ROUTERS } from "../../../../utils/router";
 import { useNavigate } from "react-router-dom";
 import logo from "../../../../img/cropedLogo.png";
 import axios from "axios";
-import { SearchBar } from "./searchbar";
-import { SearchResultsList } from "./searchResultList";
+import searchingData from "./searchingData.json";
 
 const Header = () => {
   const [menus, setMenus] = useState([
@@ -44,7 +43,16 @@ const Header = () => {
     }
   };
 
-  const [results, setResults] = useState([]);
+  const [value, setValue] = useState();
+  const [data, setData] = useState([]);
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setValue(value);
+
+    // Lọc dữ liệu từ data.json thay vì gọi API
+    const filteredData = searchingData.filter(item => item.title.startsWith(value));
+    setData(filteredData.slice(0, 5)); // Giới hạn số lượng item hiển thị
+  };
 
   return (
     <>
@@ -75,17 +83,29 @@ const Header = () => {
       </div>
       <div className="search">
         <div className="search-bar">
-          <div className="search-field">
-            <SearchBar setResults={setResults} />
-            {<SearchResultsList results={results} />}
-            {/*results && results.length > 0 && <SearchResultsList results={results} />*/}
+          <div className="search-input">
+            <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+              <path
+                  d="M16.5769 8.03846C16.5769 12.478 12.978 16.0769 8.53846 16.0769C4.09894 16.0769 0.5 12.478 0.5 8.03846C0.5 3.59894 4.09894 0 8.53846 0C12.978 0 16.5769 3.59894 16.5769 8.03846ZM1.59915 8.03846C1.59915 11.8709 4.70598 14.9778 8.53846 14.9778C12.3709 14.9778 15.4778 11.8709 15.4778 8.03846C15.4778 4.20598 12.3709 1.09915 8.53846 1.09915C4.70598 1.09915 1.59915 4.20598 1.59915 8.03846Z"
+                  fill="#929090" />
+              <line x1="13.63" y1="13.1775" x2="20.2069" y2="19.7544" stroke="#929090" stroke-width="2" />
+            </svg>
+            <input type="text" onChange={handleChange} value={value} ref={queryInputRef}/>
           </div>
-          {/*<button className="btn-search" onClick={handleSearch}>Tìm kiếm</button>*/}
+          <button className="btn-search" onClick={handleSearch} >Tìm kiếm</button>
         </div>
-    </div>
-      
+        <div className="search_result_list">
+          {
+            value &&
+            data.map(item => (
+              <div key={item.id} className="search_item" onClick={(e) => setValue(item.title)}>
+                {item.title}
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </>
-    
   );
 };
 
