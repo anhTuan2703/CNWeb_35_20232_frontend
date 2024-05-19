71bf62574ebf90483822d14aa11412552bfb901c
Quan Văn Tuấn <117747357+tunaQVT694926@users.noreply.github.com>
2024-05-19 16:26:47 +0700

    Update
    Hoàn thiện mấy lỗi vun vặt

--- src/pages/user/homePage/index.js
+++ src/pages/user/homePage/index.js
@@ -12,38 +12,7 @@ import ImageSlider from './ImageSlider';
 import secu from "../../../img/security.png";
 import deli from "../../../img/delivery-truck.png";
 import undo from "../../../img/undo.png";
-
 import "./style.css";
-
-
-
-
-
-// const addToCart = async (item) => {
-//   try {
-//     const response = await fetch(`http://localhost:3001/api/v1/order/add-product/${item.id}`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ customer_id: customerID }), // Giả sử bạn chỉ muốn thêm 1 sản phẩm
-//     });
-//     if (!response.ok) throw new Error('Lỗi khi thêm vào giỏ hàng.');
-
-//     const result = await response.json();
-//     if (result.success) {
-//       alert('Sản phẩm đã được thêm vào giỏ hàng thành công!');
-//       return true;
-//     } else {
-//       alert('Không thể thêm sản phẩm vào giỏ hàng.');
-//       return false;
-//     }
-//   } catch (error) {
-//     console.error('Error:', error);
-//     alert('Có lỗi xảy ra khi thêm sản phẩm vào giỏ hàng.');
-//     return false;
-//   }
-// };
 
 const HomePage = () => {
   const [menuItems, setMenuItems] = useState([]);
@@ -71,39 +40,6 @@ const HomePage = () => {
       </div>
     </div>
   ));
-
-  // const ProductItem = memo(({ item }) => (
-  //   <Grid item xs={12} sm={4} md={4} lg={2} style={{margin: '10px'}}>
-  //     <Card sx={{ maxHeight: 350, maxWidth: 250 }}>
-  //       <CardMedia
-  //         component="div"
-  //         height="260px" 
-  //         image={item.image}
-  //         style={{ border: '1px solid #ddd', borderRadius: '8px'}}
-  //         sx={{ height: 200 }}
-  //       >
-  //         <ul className="product_item_pic_hover">
-  //           <li>
-  //             <Link to={`${ROUTERS.USER.PRODUCT}?productID=${item.id}`}>
-  //               <AiOutlineEye style={{ fontSize: '30px', color:'red' }} /> {/* Adjust icon size */}
-  //             </Link>
-  //           </li>
-  //           <li onClick={() => addToCart(item)}>
-  //             <AiOutlineShoppingCart style={{ fontSize: '30px', color: 'red' }} /> {/* Adjust icon size */}
-  //           </li>
-  //         </ul>
-  //       </CardMedia>
-  //       <CardContent>
-  //         <Typography variant="h6" component="div">
-  //           <Link to="">{item.name}</Link>
-  //         </Typography>
-  //         <Typography variant="h5" component="div">
-  //           {formater(item.price)}
-  //         </Typography>
-  //       </CardContent>
-  //     </Card>
-  //   </Grid>
-  // ));
 
   useEffect(() => {
     const token = localStorage.getItem('token');
