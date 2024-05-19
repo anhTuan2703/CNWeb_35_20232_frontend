69a2a2e92daffd261d626c10af86321e6e3d12f9
Tung <gamer1412all@gmail.com>
2024-05-19 15:29:34 +0700

    update homepage

--- src/pages/user/homePage/index.js
+++ src/pages/user/homePage/index.js
@@ -3,39 +3,39 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEye, AiOutlineShoppingCart } from 'react-icons/ai';
 import { ROUTERS } from '../../../utils/router';
 import { formater } from '../../../utils/formater';
-import { Grid, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import Slider from 'react-slick';
+import slidejpg1 from "../../../img/slide1.jpg"
+import slidejpg2 from "../../../img/slide2.jpg"
+import slidejpg3 from "../../../img/slide3.jpg"
+import ImageSlider from './ImageSlider';
+import secu from "../../../img/security.png";
+import deli from "../../../img/delivery-truck.png";
+import undo from "../../../img/undo.png";
+
+import "./style.css";
 
 const ProductItem = memo(({ item }) => (
-  <Grid item xs={12} sm={4} md={4} lg={2} style={{margin: '10px'}}>
-    <Card sx={{ maxHeight: 350, maxWidth: 250 }}>
-      <CardMedia
-        component="div"
-        height="260px" 
-        image={item.image}
-        style={{ border: '1px solid #ddd', borderRadius: '8px'}}
-        sx={{ height: 200 }}
-      >
+  <div className="product-grid">
+    <div className="product_item">
+      <div className="product_item_pic" style={{backgroundImage: `url(${item.image})`}}> 
         <ul className="product_item_pic_hover">
           <li>
-            <Link to={`${ROUTERS.USER.PRODUCT}?productID=${item.id}`}>
-              <AiOutlineEye style={{ fontSize: '30px', color:'red' }} /> {/* Adjust icon size */}
+            <Link to={`${ROUTERS.USER.PRODUCT}?productID=${item.id}`} target ="_blank">
+              <AiOutlineEye />
             </Link>
           </li>
           <li onClick={() => addToCart(item)}>
-            <AiOutlineShoppingCart style={{ fontSize: '30px', color: 'red' }} /> {/* Adjust icon size */}
+            <AiOutlineShoppingCart/>
           </li>
         </ul>
-      </CardMedia>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          <Link to="">{item.name}</Link>
-        </Typography>
-        <Typography variant="h5" component="div">
-          {formater(item.price)}
-        </Typography>
-      </CardContent>
-    </Card>
-  </Grid>
+      </div>
+      <div className='product_item_text'>
+        <h6>{item.name}</h6>
+        <h5>{formater(item.price)}</h5>
+      </div>
+    </div>
+  </div>
+
 ));
 
 const addToCart = async (customerID, item) => {
@@ -84,19 +84,55 @@ const HomePage = () => {
     fetchMenuItems();
   }, []);
 
+  const slides = [
+    { url: slidejpg1, title: "sld1" },
+    { url: slidejpg2, title: "sld2" },
+    { url: slidejpg3, title: "sld3" },
+  ];
+  const containerStyles = {
+    width: "800px",
+    height: "280px",
+    margin: "0 auto",
+  };
+
   return (
-    <div className="container">
-      <div className="product-grid">
-        <div className="title">
-          <h3>Sản phẩm nổi bật</h3>
-        </div>
-        <Grid container spacing={2}>
-          {menuItems.map((item) => (
-            <ProductItem key={item.id} item={item} />
-          ))}
-        </Grid>
+    <>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides}/>
       </div>
+
+      <div className="sub-banner">
+        <div className="item">
+            <img src={secu} alt=""/>
+            <div className="text">
+                <p>BẢO HÀNH</p>
+                <p>ÍT NHẤT <span>1 NĂM</span></p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src={undo} alt=""/>
+            <div className="text">
+                <p>ĐỔI/TRẢ MIỄN PHÍ</p>
+                <p>TRONG <span>30 NGÀY</span></p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src={deli} alt=""/>
+            <div className="text">
+                <p><span>MIỄN PHÍ </span>VẬN CHUYỂN</p>
+                <p>ĐƠN TỪ 499.000Đ</p>
+            </div>
+        </div>
     </div>
+
+      <div className="container container2" >
+        {menuItems.map((item) => (
+          <ProductItem key={item.id} item={item} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/pages/user/productPage/index.js
+++ src/pages/user/productPage/index.js
@@ -1,7 +1,6 @@
 import { memo, useState, useEffect } from "react";
 import React from "react";
 import { useLocation } from "react-router-dom";
-import category from "../../../data/productData";
 import { formater } from "../../../utils/formater";
 import "./style.css"; 
 
@@ -9,6 +8,7 @@ const ProductPage = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const productID = searchParams.get("productID");
+  //console.log(productID);
 
   // Hàm này sẽ thêm một item vào giỏ hàng trong cơ sở dữ liệu
   const addToCart = async (customerID, item) => {
@@ -37,41 +37,49 @@ const ProductPage = () => {
     }
   };  
 
-  const renderProduct = (pID) => {
-    for (const categoryKey in category) {
-      const products = category[categoryKey].products;
-      const foundProduct = products.find(product => product.id === pID);
-      
-      if (foundProduct) {
-        return (
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <div className="product_img_col">
-                  <img className="product_image" src={foundProduct.img} alt={foundProduct.name} />
-                </div>
-              </div>
-              <div className="col-6">
-                <div className="product_info_col">
-                  <h2 className="product-name">{foundProduct.name}</h2>
-                  <p className="product-price">Giá: {formater(foundProduct.price)}</p>
-                  <p className="product_des">Mô tả sản phẩm: {foundProduct.description}</p>
-                  <p className="product_num">{foundProduct.number} sản phẩm có sẵn</p>
-                  <button onClick={() => addToCart(foundProduct)} className="sub_btn">Đặt hàng</button>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-        );
+  const [product, setProduct] = useState(null);
+  const fetchProduct = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/product/all-product');
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
       }
+      const data = await response.json();
+      const foundProduct = data.find(item => String(item.id) === productID);
+      setProduct(foundProduct || null);
+    } catch (error) {
+      console.error('Could not fetch products:', error);
     }
-    return <p className="not-found">Product not found!</p>;
   };
 
+  useEffect(() => {
+    fetchProduct();
+  }, [productID]);
+
+  if (!product) {
+    return <div>Loading...</div>; // Show loading state or error message if product is not found
+  }
+  console.log(product.image);
+  console.log(product);
+
   return (
-    <div>
-      {renderProduct(productID)}
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div className="product_img_col">
+            {<img className="product_image" src={product.image} alt={product.name} />}
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="product_info_col">
+            <h2 className="product-name">{product.name}</h2>
+            <p className="product-price">Giá: {formater(product.price)}</p>
+            <p className="product_des">Mô tả sản phẩm: {product.description}</p>
+            <p className="product_num">{product.number} sản phẩm có sẵn</p>
+            <button onClick={() => addToCart(product)} className="sub_btn">Đặt hàng</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
